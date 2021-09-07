2264c4f1234df84ea23452463eca7bbbb82465bd
Ned Palacios <npdoesmc@gmail.com>
2021-09-07 22:27:23 +0800

    script.js/style.css: add glowing hover effect

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const menuToggle = document.querySelector('.toggle');
-      const showcase = document.querySelector('.showcase');
+const showcase = document.querySelector('.showcase');
 
-      menuToggle.addEventListener('click', () => {
-        menuToggle.classList.toggle('active');
-        showcase.classList.toggle('active');
-      })
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  showcase.classList.toggle('active');
+});
+
+// Glowing text
+const glowText = document.querySelector('.glow');
+
+glowText.addEventListener('mouseover', () => {
+  glowText.classList.add('enter-glow');
+  setTimeout(() => {
+    glowText.classList.remove('enter-glow');
+    glowText.classList.add('glowing');
+  }, 550);
+});
+
+glowText.addEventListener('mouseout', () => {
+  glowText.classList.remove('glowing');
+  glowText.classList.add('leave-glow');
+  setTimeout(() => {
+    glowText.classList.remove('leave-glow');
+  }, 500);
+});
